fcef06a0fce7f72f5441597ac9779e32e2b9225e
mlatysheva <mlatysheva@gmail.com>
2022-06-09 22:39:33 +0200

    refactor: change read function to use streams

--- src/fs/readFile.js
+++ src/fs/readFile.js
@@ -1,4 +1,4 @@
-import { readFile } from 'fs/promises';
+import * as fs from 'fs';
 import { getAbsolutePath } from '../utils/getAbsolutePath.js';
 import { doesExist } from '../utils/doesExist.js';
 import { commandClosingMsg } from '../utils/commandClosingMsg.js';
@@ -8,16 +8,18 @@ export const read = async (filePath, cwd) => {
   const doesExistPath = await doesExist(absolutePath);
   if (doesExistPath) {
     try {    
-      await readFile(absolutePath, 'utf8').then((data) => {
-      console.log(data);
-      });
+      const readableStream = fs.createReadStream(absolutePath);
+
+      readableStream.on('data', (chunk) => {
+          process.stdout.write(chunk);
+      })
       commandClosingMsg(cwd);
     } catch (error) {
-      console.log(`FS operation failed\n${error}`);
+      console.log(`\r\nOperation failed!\n${error}`);
       commandClosingMsg(cwd);
     }
   } else {
-    process.stdout.write(`No such file or directory ${filePath}\n`);
+    process.stdout.write(`\r\nSuch file ${filePath} does not exist.\n`);
     commandClosingMsg(cwd);
   }
 };
